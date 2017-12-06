bcc3efd33c3bd00aacc3cc68325ff78b7904eb7f
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-06 21:35:50 +0100

    add new endpoints

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 
 // handlers
+const image = require("./image");
 const images = require("./images");
+
+const article = require("./article");
+const articles = require("./articles");
+
 const tags = require("./tags");
 
 const wrapAsync = require("../utils/wrapAsync");
 
 const app = express();
 
+app.get("/images/:id", wrapAsync(image));
 app.get("/images", wrapAsync(images));
+app.get("/articles/:id", wrapAsync(article));
+app.get("/articles", wrapAsync(articles));
 app.get("/tags", wrapAsync(tags));
 
 module.exports = app;
