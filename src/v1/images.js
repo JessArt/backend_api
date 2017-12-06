383c907e8f5c14f2d4a3df971aa96d51238c70a2
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-06 21:37:03 +0100

    add support for type query parameter for images endpoint

--- src/v1/images.js
+++ src/v1/images.js
@@ -1,15 +1,18 @@
 const { query, count } = require("../db/connection");
 
 module.exports = async (req, res) => {
-  const { offset = 0, limit = 10, tags } = req.query;
+  const { offset = 0, limit = 10, tags, type } = req.query;
   if (!tags) {
+    const typeCond = type ? "WHERE type = ?" : "";
+    const typeParam = type ? [type] : [];
     const results = await query({
-      query: "SELECT * FROM images LIMIT ? OFFSET ?",
-      params: [Number(limit), Number(offset)]
+      query: `SELECT * FROM images ${typeCond} LIMIT ? OFFSET ?`,
+      params: typeParam.concat([Number(limit), Number(offset)])
     });
 
     const total = await count({
-      query: "SELECT COUNT(*) AS total FROM images"
+      query: `SELECT COUNT(*) AS total FROM images ${typeCond}`,
+      params: typeParam
     });
 
     res.json({
@@ -19,16 +22,18 @@ module.exports = async (req, res) => {
       data: results
     });
   } else {
+    const typeCond = type ? "AND type = ?" : "";
+    const typeParam = type ? [type] : [];
     const sqlQuery = `
     SELECT * FROM
     (SELECT * FROM images
       INNER JOIN tags_images
       ON images.id = tags_images.image_id) t
-    WHERE tag_id IN (?)
+    WHERE tag_id IN (?) ${typeCond}
     LIMIT ? OFFSET ?`;
     const results = await query({
       query: sqlQuery,
-      params: [tags, Number(limit), Number(offset)]
+      params: typeParam.concat([tags, Number(limit), Number(offset)])
     });
 
     const countQuery = `
@@ -36,10 +41,11 @@ module.exports = async (req, res) => {
     (SELECT * FROM images
       INNER JOIN tags_images
       ON images.id = tags_images.image_id) t
-    WHERE tag_id IN (?)`;
-    const total = await count({ query: countQuery, params: [tags] });
-
-    console.log(total);
+    WHERE tag_id IN (?) ${typeCond}`;
+    const total = await count({
+      query: countQuery,
+      params: typeParam.concat([tags])
+    });
 
     res.json({
       offset,
