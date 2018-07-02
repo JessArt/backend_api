2bfb91a7bb5edb3da5db615d2bc1fd9a37eaf0db
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-03 00:16:28 +0200

    fix an issue with images with tags querying

--- src/v1/images.js
+++ src/v1/images.js
@@ -1,12 +1,13 @@
 const { query, count } = require("../db/connection");
 
 module.exports = async (req, res) => {
-  const { offset = 0, limit = 10, tags, type } = req.query;
+  const { pageNumber = 0, pageSize: limit = 10, tags, type } = req.query;
+  const offset = pageNumber * limit;
   if (!tags) {
     const typeCond = type ? "WHERE type = ?" : "";
     const typeParam = type ? [type] : [];
     const results = await query({
-      query: `SELECT * FROM images ${typeCond} LIMIT ? OFFSET ?`,
+      query: `SELECT * FROM images ${typeCond} ORDER BY id DESC LIMIT ? OFFSET ?`,
       params: typeParam.concat([Number(limit), Number(offset)])
     });
 
@@ -16,42 +17,89 @@ module.exports = async (req, res) => {
     });
 
     res.json({
-      offset,
-      limit,
-      total,
+      meta: {
+        elements: results.length,
+        offset,
+        page_size: Number(limit),
+        total: total
+      },
       data: results
     });
   } else {
+    const resolvedTags = await resolveTags({ tags }).then(resolvedTags =>
+      resolvedTags.map(Number)
+    );
+
     const typeCond = type ? "AND type = ?" : "";
     const typeParam = type ? [type] : [];
+    // we get all images joined with tags
+    // so every image can appear several times with different tags
+    // the trick is that we resolve tags, e.g. unwrap, so the same
+    // image can appear with several unwrapped tags. Because of that
+    // we have a line `GROUP BY id`.
     const sqlQuery = `
     SELECT * FROM
     (SELECT * FROM images
       INNER JOIN tags_images
       ON images.id = tags_images.image_id) t
     WHERE tag_id IN (?) ${typeCond}
+    GROUP BY id
+    ORDER BY id DESC
     LIMIT ? OFFSET ?`;
     const results = await query({
       query: sqlQuery,
-      params: typeParam.concat([tags, Number(limit), Number(offset)])
+      params: [resolvedTags].concat(typeParam, Number(limit), Number(offset))
     });
 
+    // we use _almost_ the same query as above, but for some reason
+    // (which I have no idea about) `GROUP BY id` clause does not work,
+    // returning 2 (for my query) instead of ~950.
+    // However, `COUNT(DISTINCT id)` works, so it is here instead of
+    // `GROUP BY id`.
+    // TODO: post a question to StackOverflow and link it here.
     const countQuery = `
-    SELECT COUNT(*) AS total FROM
+    SELECT COUNT(DISTINCT id) AS total FROM
     (SELECT * FROM images
       INNER JOIN tags_images
       ON images.id = tags_images.image_id) t
     WHERE tag_id IN (?) ${typeCond}`;
     const total = await count({
       query: countQuery,
-      params: typeParam.concat([tags])
+      params: [resolvedTags].concat(typeParam)
     });
 
     res.json({
-      offset,
-      limit,
-      total,
+      meta: {
+        elements: results.length,
+        offset,
+        page_size: Number(limit),
+        total: total
+      },
       data: results
     });
   }
 };
+
+async function resolveTags({ tags }) {
+  const tagsArray = Array.isArray(tags) ? tags : [tags];
+
+  const resolvedTagsArray = await Promise.all(
+    tagsArray.map(async tag => {
+      const relatedTags = await query({
+        query:
+          "SELECT contained_id as tagId from tags_relations WHERE parent_id = ?",
+        params: [Number(tag)]
+      });
+
+      if (relatedTags && relatedTags.length) {
+        return resolveTags({
+          tags: relatedTags.map(({ tagId }) => tagId)
+        }).then(resolvedTags => [tag].concat(resolvedTags));
+      } else {
+        return tag;
+      }
+    })
+  );
+
+  return resolvedTagsArray.reduce((arr, value) => arr.concat(value), []);
+}
