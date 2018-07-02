c9586474867abf5e349700c04c10ed1abf9cce75
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-03 00:24:47 +0200

    add comments to wrapAsync utils function

--- src/utils/wrapAsync.js
+++ src/utils/wrapAsync.js
@@ -1,1 +1,7 @@
+// this is a simple function to wrap express' handlers
+// it expects function to return a promise. The reason for it
+// is to not to catch all errors manually, rather allow them to leak
+// through and to be able to throw errors
+// express will catch this error (since we explicitly call `next`)
+// and display a proper error.
 module.exports = fn => (req, res, next) => fn(req, res).catch(next);
