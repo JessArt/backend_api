cf39cd8fc4b34602ca5d41070d9405dadb126518
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-03 00:32:01 +0200

    update index file with mounting all endpoints

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const upload = multer();
+
 const v1 = require("./v1/index");
+const v2 = require("./v2/index");
+const login = require("./login");
 
 const app = express();
 app.use(cors());
 app.use("/v1", v1);
+app.use("/v2", v2);
+app.post("/login", upload.none(), login.request);
 
-app.listen("3000");
+app.listen("4003");
